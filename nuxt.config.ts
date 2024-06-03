f2a938f8874e09d69905ee0196dd008c412ad19e
Mirmuxsin Khamroev <millykhamroev@gmail.com>
2024-06-03 08:53:12 +0500

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,13 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
   devtools: { enabled: true },
-
+  server: {
+    // Other server configurations
+    host: 'demo.modme.local',
+  },
   modules: [
     // Remove it if you don't use Plausible analytics
-    // https://github.com/nuxt-modules/plausible
+    // https://github.com/nuxt-modulesn/plausible
     '@nuxtjs/plausible'
   ]
 })
